57b4efbc0ab9672deb748381bc4541b23a9ad64f
rgarro <rgarro@gmail.com>
2020-09-18 20:29:28 -0600

    que onda rey ...

--- ObjCTut2/ObjCTut2/main.m
+++ ObjCTut2/ObjCTut2/main.m
@@ -11,7 +11,30 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
+        NSLog(@"Que onda wey!");
+        NSString *nothing = nil;
+        NSLog(@"Location of Nil : %p",nothing);
+        //----
+        NSString *quote = @"Mas vale pajaro en mano que cien volando";
+        NSLog(@"Size of string: %d",(int)[quote length]);
+        NSLog(@"Character at 5: %c",[quote characterAtIndex:5]);
+        //----
+        char *name = "Dereck";
+        NSString *myName = [NSString stringWithFormat:@"- %s",name];
+        
+        BOOL isStringEqual = [quote isEqualToString:myName];
+        printf("Are strings equal : %d\n",isStringEqual);
+        //--
+        const char *uCString = [[myName uppercaseString]UTF8String];
+        printf("%s\n",uCString);
+        //--
+        NSString *wholeQuote = [quote stringByAppendingString:myName];
+        NSRange searchResult = [wholeQuote rangeOfString:@"Derek"];
+        if(searchResult.location == NSNotFound){
+            NSLog(@"String not found");
+        } else {
+            printf("Derek is at index %lu and is %lu long\n",searchResult.location,searchResult.length);
+        }
     }
     return 0;
 }

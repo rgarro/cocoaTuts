281a77a4dfd11d9573bab6cd7cb5623e072b6dd9
rgarro <rgarro@gmail.com>
2020-09-19 18:35:07 -0600

    project init

---
+++ ObjCTut4/ObjCTut4/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ObjCTut4
+//
+//  Created by Rolando Garro on 9/19/20.
+//  Copyright © 2020 emptyArt. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}

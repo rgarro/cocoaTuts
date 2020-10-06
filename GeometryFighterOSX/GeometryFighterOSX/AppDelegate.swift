788ffa8661a0c8ae72d667430155733be1782524
rgarro <rgarro@gmail.com>
2020-10-06 17:03:30 -0600

    initial files

---
+++ GeometryFighterOSX/GeometryFighterOSX/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  GeometryFighterOSX
+//
+//  Created by Rolando Garro on 10/6/20.
+//  Copyright © 2020 emptyArt. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+}
